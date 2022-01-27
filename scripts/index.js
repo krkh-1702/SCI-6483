c836cd0d444881aaf21a4ff5d479bb788ab162ea
Kritika Kharbanda <60071917+krkh-1702@users.noreply.github.com>
2022-01-27 12:51:39 -0500

    draft-thursday-01

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
+
+//Hiding the scroll when up
 $(".bi-arrow-up-square-fill").hide();
 
+//Scrolling button activate
 $(window).scroll(function () {
     var scroll_amount = $(window).scrollTop();
     if (scroll_amount > 50) {
@@ -9,4 +12,32 @@ $(window).scroll(function () {
         $(".bi-arrow-up-square-fill").fadeOut();
     }
 });
+
+// Video aspect ratio maintainer
+var j = 0;
+const video_width = $(".comp_video").width()
+comp_video_array = document.getElementsByClassName('comp_video')
+for (j = 0; j < comp_video_array.length; j++) {
+    comp_video_array[j].style.height = (9 / 16) * video_width + "px";
+}
+// Document ready - means the page is loaded
+$(document).ready(function () {
+    $(window).resize(function () {
+        var j = 0;
+        const video_width = $(".comp_video").width()
+        comp_video_array = document.getElementsByClassName('comp_video')
+        for (j = 0; j < comp_video_array.length; j++) {
+            comp_video_array[j].style.height = (9 / 16) * video_width + "px";
+        }
+    });
+});
+
+$('.hover_image').hover(function () {
+    $(this).addClass('hover_style');
+    $('.hover_title').addClass('hover_style');
+},
+    function () {
+        $(this).removeClass('hover_style');
+        $('.hover_title').removeClass('hover_style');
+    });
 
